dd374498d8def9e529892cfc7c4d1052e6ed9db6
GED <gimaev.ufa@gmail.com>
2023-05-25 19:38:26 +0500

    adding route to train

--- l7_classes.rb
+++ l7_classes.rb
@@ -7,25 +7,25 @@
 +Может отправлять поезда (по одному за раз, при этом, поезд удаляется из списка поездов, находящихся на станции).
 
 Класс Route (Маршрут):
-+ Имеет начальную и конечную станцию, а также список промежуточных станций. 
-+ Начальная и конечная станции указываютсся при создании маршрута, а промежуточные могут добавляться между ними.
-+ Может добавлять промежуточную станцию в список
-+ Может удалять промежуточную станцию из списка
-+ Может выводить список всех станций по-порядку от начальной до конечной
+ + Имеет начальную и конечную станцию, а также список промежуточных станций. 
+ + Начальная и конечная станции указываются при создании маршрута, а промежуточные могут добавляться между ними.
+ + Может добавлять промежуточную станцию в список
+ + Может удалять промежуточную станцию из списка
+ + Может выводить список всех станций по-порядку от начальной до конечной
 
 Класс Train (Поезд):
-+Имеет номер (произвольная строка) и тип (грузовой, пассажирский) и количество вагонов, эти данные указываются при создании экземпляра класса
-+Может набирать скорость
-+Может возвращать текущую скорость
-+Может тормозить (сбрасывать скорость до нуля)
-+Может возвращать количество вагонов
-+Может прицеплять/отцеплять вагоны (по одному вагону за операцию, метод просто увеличивает или уменьшает количество вагонов). 
++ Имеет номер (произвольная строка) и тип (грузовой, пассажирский) и количество вагонов, эти данные указываются при создании экземпляра класса
++ Может набирать скорость
++ Может возвращать текущую скорость
++ Может тормозить (сбрасывать скорость до нуля)
++ Может возвращать количество вагонов
++ Может прицеплять/отцеплять вагоны (по одному вагону за операцию, метод просто увеличивает или уменьшает количество вагонов). 
 + Прицепка/отцепка      вагонов может осуществляться только если поезд не движется.
-+Может принимать маршрут следования (объект класса Route). 
-+ При назначении маршрута поезду, поезд автоматически помещается на первую станцию в маршруте.
-+ Может перемещаться между станциями, указанными в маршруте. Перемещение возможно вперед и назад, но только на 1 станцию за раз.
-Возвращать предыдущую станцию, текущую, следующую, на основе маршрута
-rescue => exception
+  Может принимать маршрут следования (объект класса Route). 
+  При назначении маршрута поезду, поезд автоматически помещается на первую станцию в маршруте.
+  Может перемещаться между станциями, указанными в маршруте. Перемещение возможно вперед и назад, но только на 1 станцию за раз.
+  Возвращать предыдущую станцию, текущую, следующую, на основе маршрута
+
 =end
 
 class Station
@@ -63,59 +63,44 @@ class Station
 end
 
 class Route
-
     attr_reader :route
-
     def initialize(departure_point, destination_point)
-        @departure_point = departure
-        @destination_point = destination
-        if departure_point.class == Station && destination_point == Station
+        @departure_point = departure_point
+        @destination_point = destination_point
         @route = [departure_point, destination_point]
-        else
-            puts "There is no station that you require"
-        end
     end
     def add_way_station(station)
-        if station.class == Station
             @route.insert(-2, station)
-          else
-            puts "There is no station that you require"
-          end
     end
     def exclude_station(station)
-        @stations.delete(station)
+        @route.delete(station)
       end
     def show_route
-        @route.each {|station| puts "#{station.name}"} 
+        @route.each {|station| print "this route has following stations: #{station}, "} 
     end
 end
 
-
 class Train
-    
     attr_reader :train_number
-    attr_reader :train_type
-    attr_reader :train_wagons
-    attr_reader :speed
-    attr_accessor :initial_station
+    attr_accessor :train_type
+    attr_accessor :wagons
+    attr_accessor :speed
 
-    def initialize(train_number, train_type, initial_station)
+    def initialize(train_number, train_type)
         @train_number = train_number
         @train_type = train_type
         @wagons = 0
         @speed = 0
-        if initial_station.class == Station
-            @initial_station = initial_station
-            else
-                puts "There is no station that you require"
-        end
+        @routes = []
     end
-
     def go
         self.speed = 10
     end
     def speed_up
         self.speed += 10
+    end
+    def show_speed
+        puts "current speed: #{@speed}"
     end
     def speed_down
         if @speed == 0
@@ -126,10 +111,6 @@ class Train
     def stop #added just in case
         self.speed = 0
     end
-    def show_speed
-        puts "current speed: #{@speed}"
-    end
-
     def hook_wagons
         self.wagons += 1 if @speed == 0
         puts "the wagon hooked successfully"
@@ -147,13 +128,12 @@ class Train
     end
     
     def set_route(route)
-        if route.class == Route
-          @route = route
-          @current_station = route.stations[0]
-        else
-          puts "There is no route that you require"
-        end
+        @routes << route
     end
+    def show_route
+        @routes.each {|station| puts station.route}
+    end
+
     def move_forward(station)
         @move_forward = @route[@route.index(@current_station) + 1]
     end
@@ -165,6 +145,4 @@ class Train
         puts "Our train follows from #{@move_back} station to #{@move_forward} station"
         puts "At the moment our train is located at #{@current_station} station"
     end
-
-
 end
