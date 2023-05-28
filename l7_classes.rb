202b565e1b649496e49a40b1e7703659953ec5bf
GED <gimaev.ufa@gmail.com>
2023-05-28 15:42:32 +0500

    refactoring is done, all functions work according to the terms of reference

--- l7_classes.rb
+++ l7_classes.rb
@@ -25,166 +25,127 @@
 + При назначении маршрута поезду, поезд автоматически помещается на первую станцию в маршруте.
 + Может перемещаться между станциями, указанными в маршруте. Перемещение возможно вперед и назад, но только на 1 станцию за раз.
 + Возвращать предыдущую станцию, текущую, следующую, на основе маршрута
-
 =end
 
 class Station
-    attr_reader :station_name
-    attr_accessor :trains
+  attr_reader :name, :trains
 
-    def initialize(station_name)
-        @station_name = station_name 
-        @trains = {}
-        @trains_on_the_station = []
-        @train_type = []
-    end
+  def initialize(name)
+    @name = name 
+    @trains = []
+  end
 
-    def train_arrival(train_number, train_type, wagons)
-        @trains_on_the_station << train_number
-        @train_type << train_type
-        #@trains[train_number] = {train_type: train_type, wagons: wagons} # just in case
-    end
+  def train_arrival(train)
+    @trains << train
+  end
 
-    def show_trains_on_the_station
-        @trains_on_the_station.each do|number| 
-            puts "#{number}"
-        end
-    end
+  def departure(train)
+    @trains.delete(train)
+  end
 
-    def show_train_types
-        @train_type.each do|type| 
-            puts "#{type}"
-        end
-    end
-    
-    def departure(train_number)
-        @trains_on_station.delete(train_number)
-    end
+  def show_trains_by_type(type)
+    @trains.select{|train| train.type == type}
+  end
 end
 
 class Route
-    attr_reader :route
-    attr_reader :stations
-    def initialize(departure_point, destination_point)
-        @departure_point = departure_point
-        @destination_point = destination_point
-        @stations = [departure_point, destination_point]
-    end
-    def add_way_station(station)
-            @stations.insert(-2, station)
-    end
-    def exclude_station(station)
-        @stations.delete(station)
-      end
-    def show_route_stations
-        print "this route has following stations: #{@stations}"
-    end
+  attr_reader :route, :stations
+
+  def initialize(departure_point, destination_point)
+    @departure_point = departure_point
+    @destination_point = destination_point
+    @stations = [departure_point, destination_point]
+  end
+
+  def add_way_station(station)
+    @stations.insert(-2, station)
+  end
+
+  def delete_station(station)
+    @stations.delete(station)
+  end
+
+  def show_route_stations
+    @stations
+  end
 end
 
 class Train
-    attr_reader :train_number
-    attr_accessor :train_type
-    attr_accessor :wagons
-    attr_accessor :speed
-    attr_accessor :current_station
+  attr_reader :number, :type
 
+  def initialize(number, type)  
+    @number = number
+    @type = type
+  end
 
-    def initialize(train_number, train_type)
-        @train_number = train_number
-        @train_type = train_type
-        @wagons = 0
-        @speed = 0
-        @routes = []
-        @current_route = 0
-        @current_station = 0
-    end
-    def go
-        self.speed = 10
-    end
-    def speed_up
-        self.speed += 10
-    end
-    def show_speed
-        puts "current speed: #{@speed}"
-    end
-    def speed_down
-        if @speed == 0
-            puts "speed down is unavailable, due to the train has been already stoped"
-        self.speed -= 10
-        end
-    end
-    def stop #added just in case
-        self.speed = 0
-    end
-    def hook_wagons
-        self.wagons += 1 if @speed == 0
-        puts "the wagon hooked successfully"
-    end
-    def unhook_wagons
-        if @wagons <= 0 || @speed == 0
-            puts "unhooking is unavailable"
-        else
-            @wagons -= 1 
-            puts "the unhooking of the wagon was successful"
-        end
-    end
-    def show_wagons
-        puts "current number of the wagons: #{@wagons}"
-    end
-    
-    def set_route(route)
-        @routes << route
-        @current_station = route.stations[0]
-        puts "#{@current_station}"
+  def go
+    @speed = 10
+  end
 
-    end
-    def show_train_route
-        @routes.each {|station| puts station.route}
-    end
+  def speed_up
+    @speed += 10
+  end
 
-    def move_forward(route)
-        a = route.stations.index(@current_station) + 1
-        b = route.stations.length-1
-        if a > b
-            puts "this is the last station"
-        else
-            @current_station = route.stations[a]
-        end
-        
-        #@current_station = @next_station
+  def current_speed
+    @speed
+  end
 
-    end
-    def move_back(route)
-        c = route.stations.index(@current_station) - 1
-        if c < 0
-            puts "this is the first station"
-        else
-            @current_station = route.stations[c]
-        end
-        
-    end
-    def show_current_location(route)
-        puts "Dear passangers!"
+  def speed_down
+    @speed -= 10 if @speed > 0
+  end
 
-        puts "At the moment our station is: #{@current_station}"
+  def stop
+    @speed = 0
+  end
 
-        c = route.stations.index(@current_station) - 1
-        if c < 0
-            puts "This is the first station, please be ready for the departure"
-        else
-            previous_station = route.stations[route.stations.index(@current_station) - 1] 
-            puts "The previous station was: #{previous_station}"
-        end
-        a = route.stations.index(@current_station) + 1
-        b = route.stations.length-1
-        if a > b
-            puts "This is the last station, please check all your belongings with you"
-        else
-            next_station = route.stations[route.stations.index(@current_station) + 1]
-            puts "The next station will be: #{next_station}"
-        end
+  def hook_wagons
+    @wagons = 0
+    @wagons = @wagons + 1 if @speed == 0
+  end
 
-    end
+  def unhook_wagons
+    @wagons -= 1 if @wagons >= 0 || @speed == 0
+  end
+
+  def show_wagons
+    @wagons
+  end
+
+  def set_route(route)
+    @route = route
+    @current_station_index = 0
+  end
+
+  def show_train_route
+    @route.stations
+  end
+
+  def move_forward
+    @current_station_index += 1 if next_station
+  end
+
+  def move_back
+    @current_station_index -= 1 if previous_station
+  end
+
+  def current_station
+    @route.stations[@current_station_index]
+  end
+
+  def next_station
+    @route.stations[@current_station_index + 1]
+  end
+
+def previous_station
+    @route.stations[@current_station_index - 1]
+  end
+
+#for testing only
+  def location
+    puts"previous: #{@route.stations[@current_station_index - 1]}"
+    puts"current: #{@route.stations[@current_station_index]}"
+    puts"next: #{@route.stations[@current_station_index + 1]}"
+  end
 end
 
 station1 = Station.new('Ufa')
@@ -210,8 +171,11 @@ route1.add_way_station('Vladimir')
 route1.add_way_station('Nigniynovgorod')
 train1.set_route(route1)
 train1.show_train_route
-train1.move_back(route1)
-train1.show_current_location(route1)
+train1.move_back
+train1.location #just for testing
+station1.train_arrival(train1)
+station1.train_arrival(train2)
+station1.show_trains_by_type("cargo")
 
 
 
