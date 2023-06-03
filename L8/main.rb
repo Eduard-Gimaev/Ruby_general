1946cf8589c117ca655e87054b0499bdc4a97705
GED <gimaev.ufa@gmail.com>
2023-06-03 22:02:53 +0500

    added function for route, but interpolation for points still doesn't work

--- L8/main.rb
+++ L8/main.rb
@@ -22,13 +22,14 @@ class RailRoadOperator
       puts "Insert your command: "
       puts "1. Create a station"
       puts "2. Create a train"
-      puts "3. Create a route and determine the list of stations"
-      puts "4. Set a route to the train"
-      puts "5. Hook wagon to the train"
-      puts "6. Unhook wagons form the train"
-      puts "7. Move the train along the route forward or back"
-      puts "8. View the list of stations and the list of trains at the station"
-      puts "9. Close menu"
+      puts "3. Create a route"
+      puts "4. Determine the list of stations"
+      puts "5. Set a route to the train"
+      puts "6. Hook wagon to the train"
+      puts "7. Unhook wagons form the train"
+      puts "8. Move the train along the route forward or back"
+      puts "9. View the list of stations and the list of trains at the station"
+      puts "10. Close menu"
 
       command = gets.chomp.to_i
 
@@ -36,12 +37,13 @@ class RailRoadOperator
       when 1 then create_station
       when 2 then create_train
       when 3 then create_route
-      when 4 then set_route
-      when 5 then hook_wagons
-      when 6 then unhook_wagons
-      when 7 then train_move
-      when 8 then show_trains_on_stations
-      when 9 then break
+      when 4 then seed_route
+      when 5 then set_route
+      when 6 then hook_wagons
+      when 7 then unhook_wagons
+      when 8 then train_move
+      when 9 then show_trains_on_stations
+      when 10 then break
       else
         puts "there is no such command, try again"
       end
@@ -53,10 +55,10 @@ end
 def create_station
   puts "Enter the name of the station:"
   name = gets.chomp
-      @all_stations << Station.new(name)
-      puts "The station \"#{name}\" has been created"
-      puts "========="
-      puts @all_stations
+  @all_stations << Station.new(name)
+  puts "The station \"#{name}\" has been created"
+  puts "List os station"
+  @all_stations.each_with_index {|val, index| puts "#{index}. #{val.name} " }
 end
 
 #"2. Create a train"
@@ -75,10 +77,10 @@ def create_train
   else
     puts "Enter the correct type of the train"
   end
-  puts @all_trains
+  @all_trains.each_with_index {|val, index| puts "#{index}. #{val.number} - #{val.type}" }
 end
 
-#"3. Create a route and determine the list of stations"
+#"3. Create a route"
 def create_route
   puts "departure_point:"
   departure_point = gets.chomp
@@ -86,22 +88,30 @@ def create_route
   destination_point = gets.chomp
   @all_routes << Route.new(departure_point, destination_point)
   puts "The route \"#{departure_point}\" - \"#{destination_point}\" has been created"
-  puts "========="
-  puts @all_stations
-
+  @all_routes.each_with_index {|val, index| puts "#{index}. #{val}" }
 end
-#"4. Set a route to the train"
+
+#"4. Determine the list of stations"
+def seed_route
+  route = route_by_points
+  station = station_by_name
+  route.add_station(station)
+  puts "The way station #{station} has been added"
+  puts "#{route}"
+end
+
+#"5. Set a route to the train"
 def set_route
 end
 
-#"5. Hook wagon to the train"
+#"6. Hook wagon to the train"
 def hook_wagons
   train = train_by_num
   train.hook_wagons
   puts "Perfect, we have #{train.show_wagons.length} wagon(s) now"
 end
 
-#"6. Unhook wagons form the train"
+#"7. Unhook wagons form the train"
 def unhook_wagons
   train = train_by_num
   puts "We have #{train.show_wagons.length} which one do you want to unhook?"
@@ -110,7 +120,7 @@ def unhook_wagons
   puts "We have #{train.show_wagons.length} wagon(s) left now"
 end
 
-#"7. Move the train along the route forward or back"
+#"8. Move the train along the route forward or back"
 def train_move
   train = train_by_num
   puts "where are you going to move: forward or back?"
@@ -125,19 +135,31 @@ def train_move
   end
 end
 
-#"8. View the list of stations and the list of trains at the station"
+#"9. View the list of stations and the list of trains at the station"
 def show_trains_on_stations
+  #@all_stations.each_with_index {|val, index| puts "#{index}. #{val.name} " }
 end
 
 
 def train_by_num
-  puts "We have #{@all_trains.length} train(s), choose one:"
-  number = gets.chomp.to_i
-  train = @all_trains[number - 1]
+  puts "This is the list of the trains:"
+  @all_stations.each_with_index {|val, index| puts "#{index}. #{val.number}" }
+  puts "Enter one of the #{@all_trains.length} train(s):"
+  num = gets.chomp.to_i
+  train = @all_trains[num - 1]
 end
 
 def station_by_name
-  puts "We have bunch of the station: #{@all_stations.length}, choose one:"
-  name = gets.chomp.to_i
-  train = @all_stations[name - 1]
+  puts "This is the list of the stations:"
+  @all_stations.each_with_index {|val, index| puts "#{index}. #{val.name}" }
+  puts "Enter the name of the station from the list above:"
+  station = gets.chomp
+end
+
+def route_by_points
+  puts "This is the list of the routes:"
+  @all_routes.each_with_index {|val, index| puts "#{index}. #{val.destination_point}" }
+  puts "Enter number of the route:"
+  num = gets.chomp.to_i
+  route = @all_routes[num - 1]
 end
